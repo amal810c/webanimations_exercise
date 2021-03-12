f991884864e434c299d15485c39268fda272c2e2
amaliegrøn <amal810c@stud.kea.dk>
2021-03-12 18:17:23 +0100

    exercise 7_1 and 7_2 try

--- 7_stars_1.js
+++ 7_stars_1.js
@@ -5,20 +5,30 @@ const screen = document.querySelector("body").getBoundingClientRect();
 document.querySelectorAll(".star").forEach((star) => {
   const endX = (Math.random() - 0.5) * screen.width * 2;
   const endY = (Math.random() - 0.5) * screen.height * 2;
+  let randomDuration = Math.floor(Math.random() * (5000 - 3000) + 3000);
+  let randomOpacity = Math.floor(Math.random() * (2 - 0) + 0);
+  let randomScale = Math.floor(Math.random() * (1.5 - 0.1) + 0.1);
 
   // create keyframes to animate (translate) each star to a random end X and Y
-  const translateStar = {
-    duration: 1,
-    interations: Infinity,
-    //composite: "add",
+  const starProperties = {
+    duration: randomDuration,
+    iterations: 1,
+    composite: "add",
   };
 
-  const translateStarKeyframes = [
+  const starKeyframes = [
     // prettier-ignore
-    {transform: `translate(${endX}px,${endY}px)`},
+    {  transform: `translate(${endX}px,${endY}px) scale(${randomScale})`,
+  opacity: randomOpacity},
   ];
 
-  star.animate(translateStarKeyframes, translateStar);
+  const starAnimation = star.animate(starKeyframes, starProperties);
+
+  starAnimation.onfinish = () => {
+    starAnimation.cancel();
+    starAnimation.currentTime = 0;
+    starAnimation.play();
+  };
 
   // Also make the duration random - and why not also animate opacity and scale (to and from random values)
 });
